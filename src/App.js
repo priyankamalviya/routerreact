6feacb0e2ff687683edfc4d9f8e3e30e699a9947
Priyanka Malviya <priyanka_malviya@icloud.com>
2016-11-14 14:37:54 -0800

    use of named components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import React from 'react';
-import {Router, Route, Link, hashHistory} from 'react-router';
+import {Router, Route, Link, IndexRoute, hashHistory} from 'react-router';
 
 
-const Message = (props) => <div><h1> {props.params.message || "Hello"} </h1><Links /></div>;
+const Home = () => <h1>Home</h1>
+const HomeBody = () => <div>This is the home body</div>
+
+const Other = () => <h1>Other</h1>
+const OtherBody = () => <div>This is the other body</div>
+
+const Container = (props) =>
+  <div>
+{props.header}{props.body} <Links />
+  </div>
+
 
 const Links = () =>
-  <nav>
-    <Link  to="/"> Hello </Link>
-    <Link  to="/Hi"> Hi </Link>
-    <Link  to="/Yo"> Yo </Link>
-  </nav>
+<nav>
+  <Link to="/">Home</Link>
+  <Link to="/other">Other</Link>
+</nav>
 
 
 class App extends React.Component {
   render(){
     return (
       <Router history={ hashHistory }>
-        <Route path="/(:message)" component={Message}>
+        <Route path="/" component={Container}>
+          <IndexRoute components={{header: Home, body: HomeBody}} />
+
+          <Route path="/other" components={{header:Other, body: OtherBody}} />
         </Route>
       </Router>
     );
