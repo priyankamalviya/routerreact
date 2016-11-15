ec81a6e37427747e0782ce1589d0734676d4d61f
Priyanka Malviya <priyanka_malviya@icloud.com>
2016-11-14 19:48:43 -0800

    working with query parameters

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
 import React from 'react';
-import {Router, Route, Link, IndexRoute, hashHistory} from 'react-router';
+import {Router, Route, Link, hashHistory} from 'react-router';
 
+const Page = (props) => <div><h1>{props.location.query.message || 'Hello'}</h1><Links /></div>
 
-const Home = () => <h1>Home</h1>
-const HomeBody = () => <div>This is the home body</div>
-
-const Other = () => <h1>Other</h1>
-const OtherBody = () => <div>This is the other body</div>
-
-const Container = (props) =>
-  <div>
-{props.header}{props.body} <Links />
-  </div>
-
-
-const Links = () =>
-<nav>
-  <Link to="/">Home</Link>
-  <Link to="/other">Other</Link>
+const Links = () => <nav>
+  <Link to={{pathname: '/',query:{message: 'Yo' } }}>Yo</Link>
 </nav>
 
-
-class App extends React.Component {
+class App extends React.Component{
   render(){
-    return (
+    return(
       <Router history={ hashHistory }>
-        <Route path="/" component={Container}>
-          <IndexRoute components={{header: Home, body: HomeBody}} />
-
-          <Route path="/other" components={{header:Other, body: OtherBody}} />
-        </Route>
+        <Route path="/" component={Page}></Route>
       </Router>
-    );
+    )
   }
 }
-
-
 export default App;
